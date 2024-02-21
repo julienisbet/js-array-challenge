83b16aeb01b3338f5ad0c4b1ef154a0a14e24cb2
Julie Nisbet <jmnisbet@gmail.com>
2024-02-20 17:37:11 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+// Challenge #1
+// For this challenge, read the data from data.json
+// and generate a list of users who were born after 1975
+// The list of users should include their first name and last name
+// separated by a space
+
+const data = require("./data.json");
+
+// Challenge #2 - Generations
+// The generations are roughly defined as the following:
+// Silent Generation: 1928-1945
+// Baby Boomers: 1946 -1964
+// Gen X: 1965 -1980
+// Millenials: 1981-1996
+// Gen Z:1996-2012
+// Gen Alpha: 2013-2025
+// Generate a map of each of the users that fall into these buckets
+// Final output should follow this format
+// {
+//   'Silent Generation': [
+//     'Mayne Redit (1939)',
+//     'Alfred Stollard (1943)',
+//     'Shae Diem (1941)',
+//     'Malachi Zelner (1943)',
+//     'Renault Dannatt (1939)'
+//   ],
+//   'Baby Boomers': [
+//     'Colene Pecht (1964)',
+//     'Winonah Screeton (1927)',
+//     'Anthony Gallyhaock (1959)',
+//     'Arlina Johantges (1950)'
+//   ],...
+// }
